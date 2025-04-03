79cba1a0efab3a91f236aad5db377b4f50b99c77
Tactlesst <nasefaquiatan@gmail.com>
2025-04-03 15:14:26 +0800

    v4
    v4

--- sosemergency/pages/index.js
+++ sosemergency/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-6">Welcome to SOS</h1>
+      <h1 className="text-3xl font-bold mb-6">Welcome to OJT MONITORING</h1>
       <p className="text-gray-600 mb-4">Tap below to log in or register.</p>
 
       <button
